e8ef4ef9417d8f291de688ca50e03681333a47a2
qwerty432 <dimichkit@ukr.net>
2018-05-19 13:39:06 +0300

    updated server

--- Tutorial5/run_server.py
+++ Tutorial5/run_server.py
@@ -2,12 +2,13 @@ import flask
 from telebot import types
 from config import *
 from bot_handlers import bot
+import os
 
 server = flask.Flask(__name__)
 
 
-@server.route('/' + BOT_TOKEN, methods=['POST'])
-def getMessage():
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
     bot.process_new_updates([types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
     return "!", 200
 
@@ -15,5 +16,9 @@ def getMessage():
 @server.route('/', methods=["GET"])
 def index():
     bot.remove_webhook()
-    bot.set_webhook(url="https://{}.herokuapp.com/{}".format(APP_NAME, BOT_TOKEN))
-    return "!", 200
+    bot.set_webhook(url="https://{}.herokuapp.com/{}".format(APP_NAME, TOKEN))
+    return "Hello from Heroku!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
